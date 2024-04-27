916370d365646805ff1991fcbbc037ffb9ea8d92
rakib <rakib135836@gmail.com>
2024-04-27 10:40:12 +0600

    slider added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
+import Slider from "../../components/Slider/Slider";
 
 
 const Home = () => {
     return (
         <div>
             <h1>this is Home</h1>
+            <Slider></Slider>
         </div>
     );
 };
---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,38 @@
+
+
+const Slider = () => {
+    return (
+        <div className="carousel w-full h-96 my-4">
+            <div id="slide1" className="carousel-item relative w-full">
+                <img src="https://i.ibb.co/NSG5khv/ebadur-rehman-kaium-GBYWXNWO81-A-unsplash.jpg" className="w-full h-96" />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide4" className="btn btn-circle">❮</a>
+                    <a href="#slide2" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide2" className="carousel-item relative w-full">
+                <img src="https://i.ibb.co/7gLWhp7/Indonesia.jpg" className="w-full h-96" />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide1" className="btn btn-circle">❮</a>
+                    <a href="#slide3" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide3" className="carousel-item relative w-full">
+                <img src="https://i.ibb.co/JkC98Sk/Malaysia.jpg" className="w-full h-96" />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide2" className="btn btn-circle">❮</a>
+                    <a href="#slide4" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide4" className="carousel-item relative w-full">
+                <img src="https://i.ibb.co/tx8KYFq/Vietnam.jpg" className="w-full h-96" />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide3" className="btn btn-circle">❮</a>
+                    <a href="#slide1" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Slider;
