9b2f9d4e321b60f67616beafd03c7867bd3afdd0
rakib <rakib135836@gmail.com>
2024-04-28 17:51:01 +0600

    form data successfully sent to database

--- src/Pages/AddTouristsSpot/AddTouristsSpot.jsx
+++ src/Pages/AddTouristsSpot/AddTouristsSpot.jsx
@@ -1,15 +1,64 @@
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const AddTouristsSpot = () => {
 
+    const notify = () => toast("data successfully added to database ");
 
 
-    
+    const handleAddTouristSpot = event => {
+        event.preventDefault();
+        const form=event.target;
+
+        const image =form.image.value;
+        const spotName =form.spotName.value;
+        const countryName =form.countryName.value;
+        const location =form.location.value;
+        const shortDescription =form.shortDescription.value;
+        const averageCost =form.averageCost.value;
+        const seasonality =form.seasonality.value;
+        const travelTime =form.travelTime.value;
+        const visitors =form.visitors.value;
+        const email =form.email.value;
+        const userName =form.userName.value;
+        
+
+        const newTouristSpot={image,spotName,countryName,location,shortDescription,averageCost,seasonality,travelTime,visitors,email,userName};
+        console.log(newTouristSpot);
+
+
+
+         // sending data to the server 
+
+    fetch('http://localhost:5000/new-spot',{
+        method:'POST',
+        headers:{
+            'content-type':'application/json'
+        },
+        body:JSON.stringify(newTouristSpot)
+
+    })
+    .then(res=>res.json())
+    .then(data=>{
+        console.log(data)
+        if(data.insertedId){
+            notify('data added to data base ');
+        }
+    })
+
+
+
+    }
+
+   
+
+
     return (
         <div className="p-24 bg-gray-100 rounded-lg my-5">
 
             <h1 className="text-3xl font-bold text-center my-7">Add tourist spot </h1>
-            <form>
+
+            <form onSubmit={handleAddTouristSpot}>
 
                 {/* input row - name and spot name  */}
                 <div className="flex flex-col lg:flex-row gap-5 mb-3">
@@ -19,15 +68,15 @@ const AddTouristsSpot = () => {
                             <span className="label-text">Image Url</span>
                         </div>
                         <input type="text" name="image" placeholder="your image url" className="input input-bordered w-full " />
-                        
+
                     </label>
 
                     <label className="form-control lg:w-1/2 ">
                         <div className="label">
                             <span className="label-text">Tourist spot name</span>
                         </div>
-                        <input type="text" name="spot-name" placeholder="write tourist spot name here " className="input input-bordered w-full " />
-                        
+                        <input type="text" name="spotName" placeholder="write tourist spot name here " className="input input-bordered w-full " />
+
                     </label>
 
                 </div>
@@ -38,8 +87,8 @@ const AddTouristsSpot = () => {
                         <div className="label">
                             <span className="label-text">Country Name</span>
                         </div>
-                        <input type="text" name="country-name" placeholder="Add country name " className="input input-bordered w-full " />
-                        
+                        <input type="text" name="countryName" placeholder="Add country name " className="input input-bordered w-full " />
+
                     </label>
 
                     <label className="form-control lg:w-1/2  ">
@@ -47,7 +96,7 @@ const AddTouristsSpot = () => {
                             <span className="label-text">Location</span>
                         </div>
                         <input type="text" name="location" placeholder="Add spot location " className="input input-bordered w-full " />
-                        
+
                     </label>
 
                 </div>
@@ -58,16 +107,16 @@ const AddTouristsSpot = () => {
                         <div className="label">
                             <span className="label-text">Short Description</span>
                         </div>
-                        <input type="text" name="short-description" placeholder="Add a short description" className="input input-bordered w-full " />
-                        
+                        <input type="text" name="shortDescription" placeholder="Add a short description" className="input input-bordered w-full " />
+
                     </label>
 
                     <label className="form-control lg:w-1/2 ">
                         <div className="label">
                             <span className="label-text">Average Cost</span>
                         </div>
-                        <input type="text" name="average-cost" placeholder="how much it will cost " className="input input-bordered w-full " />
-                        
+                        <input type="text" name="averageCost" placeholder="how much it will cost " className="input input-bordered w-full " />
+
                     </label>
 
                 </div>
@@ -79,15 +128,15 @@ const AddTouristsSpot = () => {
                             <span className="label-text">Seasonality</span>
                         </div>
                         <input type="text" name="seasonality" placeholder="which is best to visit here" className="input input-bordered w-full " />
-                        
+
                     </label>
 
                     <label className="form-control lg:w-1/2 ">
                         <div className="label">
                             <span className="label-text">Travel Time</span>
                         </div>
-                        <input type="text" name="travel-time" placeholder="your travel time " className="input input-bordered w-full" />
-                        
+                        <input type="text" name="travelTime" placeholder="your travel time " className="input input-bordered w-full" />
+
                     </label>
 
                 </div>
@@ -99,7 +148,7 @@ const AddTouristsSpot = () => {
                             <span className="label-text">Visitors Per Year</span>
                         </div>
                         <input type="text" name="visitors" placeholder="Visitors Per Year" className="input input-bordered w-full" />
-                        
+
                     </label>
 
                     <label className="form-control lg:w-1/2 ">
@@ -107,7 +156,7 @@ const AddTouristsSpot = () => {
                             <span className="label-text">Email</span>
                         </div>
                         <input type="email" name="email" placeholder="your-email@gmail.com " className="input input-bordered w-full " />
-                        
+
                     </label>
 
                 </div>
@@ -118,8 +167,8 @@ const AddTouristsSpot = () => {
                         <div className="label">
                             <span className="label-text">User name</span>
                         </div>
-                        <input type="text" name="user-name" placeholder="write down your name " className="input input-bordered w-full " />
-                        
+                        <input type="text" name="userName" placeholder="write down your name " className="input input-bordered w-full " />
+
                     </label>
 
                 </div>
@@ -127,6 +176,7 @@ const AddTouristsSpot = () => {
                 <input type="submit" value="Add spot" className="btn btn-block my-7" />
 
             </form>
+            <ToastContainer />
         </div>
     );
 };
